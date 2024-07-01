ce04b78b87016c514b92ac2a573ccdfecedc8f6b
ags <a.g.samkin@gmail.com>
2024-07-01 21:23:07 +0300

    Initial commit

---
+++ src/main/java/com/example/CqrsBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsBankingAppApplication.class, args);
+	}
+
+}
